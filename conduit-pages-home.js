dc193f348dc9e884561af85bcb52243b92e29fa4
Guillermo Carrillo <gach87@gmail.com>
2020-07-11 09:58:10 -0400

    Simplified components

--- conduit-pages-home.js
+++ conduit-pages-home.js
@@ -1,26 +1,21 @@
 angular.module("app").component("conduitPagesHome", {
-  template: `
-  <div class="home-page">
-    <div class="banner">
-      <div class="container">
-        <h1 class="logo-font">conduit</h1>
-        <p>A place to share your knowledge.</p>
+  template: html`
+    <div class="home-page">
+      <div class="banner">
+        <div class="container">
+          <h1 class="logo-font">conduit</h1>
+          <p>A place to share your knowledge.</p>
+        </div>
       </div>
-    </div>
-    <div class="container page">
-      <div class="row">
-        <div class="col-md-9">
-          <conduit-articles-feeds
-            feeds="$ctrl.feeds"
-            selected="$ctrl.selectedFeed"
-            on-select="$ctrl.onFeedSelected"
-          ></conduit-articles-feeds>
-
-          <conduit-articles-list articles="$ctrl.articles">
-            <conduit-articles-list-item
-               ng-repeat="article in $ctrl.articles"
-               article="article"
-            >
+      <div class="container page">
+        <div class="row">
+          <div class="col-md-9">
+            <conduit-articles-feeds
+              feeds="$ctrl.feeds"
+              selected="$ctrl.selectedFeed"
+              on-select="$ctrl.onFeedSelected"
+            ></conduit-articles-feeds>
+            <div ng-repeat="article in $ctrl.articles">
               <conduit-articles-preview article="article">
                 <conduit-articles-meta article="article">
                   <conduit-buttons-favorite
@@ -29,19 +24,18 @@ angular.module("app").component("conduitPagesHome", {
                   ></conduit-buttons-favorite>
                 </conduit-articles-meta>
               </conduit-articles-preview>
-            </conduit-articles-list-item>
-          </conduit-articles-list>
-        </div>
-        <div class="col-md-3">
-          <conduit-tags-popular
-            tags="$ctrl.tags"
-            on-select="$ctrl.onTagSelected"
-          ></conduit-tags-popular>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <conduit-tags-popular
+              tags="$ctrl.tags"
+              on-select="$ctrl.onTagSelected"
+            ></conduit-tags-popular>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-`,
+  `,
   controller: function (conduiPagesHomeService) {
     var ctrl = this;
 
