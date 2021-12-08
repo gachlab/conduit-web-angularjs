b7c4412d05efe947cd0c7150eb0014cd739a79dd
Guillermo Carrillo <gach87@gmail.com>
2021-12-07 22:39:51 -0400

    Simplified articles components

--- src/components/conduit-articles-preview/index.js
+++
@@ -1,9 +0,0 @@
-export default function (dependencies) {
-	const module = dependencies.angular.module('conduit.components.articles.preview', [])
-	module.component('conduitArticlesPreview', {
-		template: dependencies.template,
-		bindings: { article: '<' },
-		transclude: true,
-	});
-	return module
-};
